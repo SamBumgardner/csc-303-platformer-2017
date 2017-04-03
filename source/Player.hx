dfc64bd2d19603e518770f135b5a83484d3de1c8
Sam Bumgardner <SamBumgardner1@gmail.com>
2017-04-03 14:04:53 -0500

    Created initial version of Player class.
    Has a basic graphic and left-right movement, but not much else.
    Gravity, collision with ground, and jumping will all be future objectives.

---
+++ source/Player.hx
@@ -0,0 +1,120 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.input.keyboard.FlxKey;
+
+/**
+ * ...
+ * @author Sam Bumgardner
+ */
+class Player extends FlxSprite 
+{
+	public var xAccel:Float = 400;
+	public var xMaxSpeed(default, set):Float;
+	
+	public var walkSpeed:Float = 100;
+	public var runSpeed:Float = 200;
+	
+	public var xSlowdown:Float = 600;
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		makeGraphic(32, 32, FlxColor.BLUE);
+		
+		xMaxSpeed = walkSpeed;
+	}
+	
+	public function set_xMaxSpeed(newXSpeed:Float):Float
+	{
+		maxVelocity.set(newXSpeed);
+		return xMaxSpeed = newXSpeed;
+	}
+	
+	public override function update(elapsed:Float):Void
+	{
+		// Determine if running
+		if (FlxG.keys.anyPressed([FlxKey.Z, FlxKey.PERIOD]))
+		{
+			xMaxSpeed = runSpeed;
+		}
+		else
+		{
+			xMaxSpeed = walkSpeed;
+		}
+		
+		// Determine direction of movement
+		var horizontalMove:Int = 0;
+		
+		if (FlxG.keys.anyPressed([FlxKey.LEFT, FlxKey.A]))
+		{
+			horizontalMove--;
+		}
+		if (FlxG.keys.anyPressed([FlxKey.RIGHT, FlxKey.D]))
+		{
+			horizontalMove++;
+		}
+		
+		// Moving Left
+		if (horizontalMove == -1)
+		{
+			if (velocity.x > 0)
+			{
+				acceleration.set(-xSlowdown + -xAccel);
+			}
+			else
+			{
+				acceleration.set(-xAccel);
+			}
+		}
+		
+		// Moving Right
+		if (horizontalMove == 1)
+		{
+			if (velocity.x < 0)
+			{
+				acceleration.set(xSlowdown + xAccel);
+			}
+			else
+			{
+				acceleration.set(xAccel);
+			}
+		}
+		
+		// Slow down if no direction held
+		if (horizontalMove == 0)
+		{
+			if (velocity.x > 0)
+			{
+				if (velocity.x <= xSlowdown * elapsed)
+				{
+					velocity.set(0);
+					acceleration.set(0);
+				}
+				else
+				{
+					acceleration.set(-xSlowdown);
+				}
+			}
+			else if (velocity.x < 0)
+			{
+				if (velocity.x >= -xSlowdown * elapsed)
+				{
+					velocity.set(0);
+					acceleration.set(0);
+				}
+				else
+				{
+					acceleration.set(xSlowdown);
+				}
+			}
+		}
+		
+		super.update(elapsed);
+	}
+	
+}
