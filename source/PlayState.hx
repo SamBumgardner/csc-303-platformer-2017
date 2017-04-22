36b4b2310b663d86686a3d41e9e687b48456ed23
CalebBreslin <jackscreed10@gmail.com>
2017-04-22 16:56:47 -0500

    Added powerup code to PlayState file.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,6 +5,9 @@ import flixel.FlxState;
 import flixel.graphics.FlxGraphic;
 import flixel.tile.FlxTilemap;
 import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import flixel.FlxObject;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState
 {
@@ -13,6 +16,7 @@ class PlayState extends FlxState
 	private var map:FlxTilemap;
 	private var player:Player;
 	public static var hud:HeadsUpDisplay;
+	public var _pUp:FlxGroup;
 	
 	override public function create():Void
 	{
@@ -44,12 +48,31 @@ class PlayState extends FlxState
 			20, 15, AssetPaths.tiles__png, 32, 32);
 		add(map);
 		add(hud);
+		
+		_pUp = new FlxGroup();
+		createPowerUp(25, 25);
+		createPowerUp(40,60);
+		add(_pUp);	
 	}
-
+	
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 		hud.update(elapsed);
 		FlxG.collide(map, player);
+		FlxG.overlap(player, _pUp, getPowerup);
+	}
+	
+	public function createPowerUp (x:Int, y:Int): Void
+	{	
+		var powerup:FlxSprite = new FlxSprite(x*8 + 3, y*8 + 2);
+		powerup.makeGraphic(32,32, FlxColor.YELLOW);
+		_pUp.add(powerup);
+	}
+	
+	private function getPowerup(Player:FlxObject, PowerUp:FlxObject):Void
+	{
+		trace("PowerUp Collected");
+		PowerUp.kill();
 	}
 }
