f2d814fd8a0d8e0a45f89b665a2f5d79eda9f269
CamYuan <camytheman@hotmail.com>
2017-05-15 05:09:34 -0500

    Item.hx -Added comments and formatted lines -Removed some unnecessary code -Added justDropped bool and timer to properly drop  and delay pickup -Adjusted drop logic to be smoother

--- source/Item.hx
+++ source/Item.hx
@@ -1,10 +1,10 @@
 package;
-import flash.display.InterpolationMethod;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
-import flixel.tweens.FlxTween;
+import haxe.Timer;
+
 
 /**
  * ...
@@ -12,40 +12,38 @@ import flixel.tweens.FlxTween;
  */
 class Item extends FlxSprite
 {
+	private var player_trace:Player;
+	
 	public var weildable:Bool = false;
 	public var equipped:Bool = false;
-	private var player_trace:Player;
-	private var item_activated:Bool = false;
+	public var justDropped:Bool = false;
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
 		acceleration.y = (cast FlxG.state).GRAVITY;
-		
 	}
 	
 	/**
 	 * Override for update function
 	 * If the player has the item equipped, Item should follow the player
+	 * and face the same direction as them
+	 * Optimally, the player would have a different sprite on equip, So this
+	 * could be considered a temporary change
 	 */
 	public override function update(elapsed:Float):Void
 	{       
 		super.update(elapsed);
 		if (equipped){
-			if (!player_trace.alive){
-				drop_item();
+			//Optimally we would set exists to false, 
+			//and change the graphic of the character to show it has been equipped
+			if (facing == FlxObject.LEFT){
+			x = player_trace.x;
 			}
-			else 
-			{	if(!item_activated){
-					if (player_trace.facing == FlxObject.LEFT){
-					x = player_trace.x;
-					}
-					else {
-					x = player_trace.x+player_trace.frameWidth-frameWidth;
-					}
-					y = player_trace.y;
-				}
+			else {
+			x = player_trace.x+player_trace.frameWidth-frameWidth;
 			}
+			y = player_trace.y;
 		}
 	}
 	
@@ -56,25 +54,29 @@ class Item extends FlxSprite
 	public function equip(player:Player){
 		equipped = true;
 		player_trace = player;
+		
 	}
 	
 	/**
-	 * Functionality to add a keypress event to drop items
-	 * If the player dies, item should drop. 
-	 */
-	
+	 * Functionality of keypress event to drop items
+	 * If the player dies, item should drop as well
+	 */	
 	public function drop_item(){
-		if (player_trace.facing == FlxObject.LEFT){
-			x -= frameWidth;
-		}
-		else {
-			x += frameWidth;
-		}
+		justDropped = true;
 		equipped = false;
+		player_trace = null;
+		velocity.y = 0;
+		acceleration.y = (cast FlxG.state).GRAVITY;
+		Timer.delay(function(){ justDropped = false; }, 1000);
 	}
 	
+	
+	/**
+	 * Functionality for keypress event to attack if an item is currently being 
+	 * weilded. Disables user input to put them in the 'attacking state', but 
+	 * still keeps their current velocity and acceleration
+	 */
 	public function attack_state(){
-		item_activated = true;
 		FlxG.keys.reset();
 		FlxG.keys.enabled = false;
 	}
